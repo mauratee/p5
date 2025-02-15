0e6cf867cc0f853e5bc2fb87303149f01c7d8510
mauratee <thomaura@gmail.com>
2025-02-14 20:47:53 -0500

    change color to lavender

--- perlinNoise.js
+++ perlinNoise.js
@@ -5,19 +5,19 @@ function setup() {
   loadPixels();
   let xoffset = 0.0;
 
-  for (let i = 0; i < pixels.length; i += 4) {
-    pixels[i] = 0;
-    pixels[i + 1] = 0;
-    pixels[i + 2] = 255;
-  }
-
   for (let x = 0; x < windowWidth; x++) {
     let yoffset = 0.0;
 
     for (let y = 0; y < windowHeight; y++) {
-      const bright = map(noise(xoffset, yoffset), 0, 1, 100, 175)
-      set(x, y, floor(bright));
-      yoffset += 0.01
+      const bright = map(noise(xoffset, yoffset), 0, 1, 0, 255)
+      let index = (x + y * width) * 4;
+
+      pixels[index] = 230;
+      pixels[index + 1] = 230;
+      pixels[index + 2] = 250;
+      pixels[index + 3] = bright;
+
+      yoffset += 0.0125
     }
     xoffset += 0.01
     
